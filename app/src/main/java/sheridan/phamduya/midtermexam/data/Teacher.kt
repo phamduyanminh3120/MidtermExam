cf6b12d0c6f8748555e908b2e5aeb5d2c95965f3
Minh Pham <phamduya@sheridancollege.ca>
2023-06-20 10:09:55 -0400

    Added more dummy data

--- app/src/main/java/sheridan/phamduya/midtermexam/data/Teacher.kt
+++ app/src/main/java/sheridan/phamduya/midtermexam/data/Teacher.kt
@@ -13,4 +13,7 @@ val teachers = listOf(
     Teacher("Pham", 30, EmployementStatus.PART_TIME, true),
     Teacher("Martin", 30, EmployementStatus.PART_TIME, false),
     Teacher("Mike", 30, EmployementStatus.SESSIONAL, false),
+    Teacher("Tylor", 30, EmployementStatus.SESSIONAL, true),
+    Teacher("Louis", 30, EmployementStatus.FULL_TIME, false),
+    Teacher("Wright", 30, EmployementStatus.PART_TIME, false),
 )
