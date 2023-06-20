0d420552230413811d89dae1ae12f66ab0bac03d
Minh Pham <phamduya@sheridancollege.ca>
2023-06-20 09:24:49 -0400

    Created a Teacher data class

---
+++ app/src/main/java/sheridan/phamduya/midtermexam/data/Teacher.kt
@@ -0,0 +1,16 @@
+package sheridan.phamduya.midtermexam.data
+
+data class Teacher(
+    val name: String,
+    val hour: Int,
+    val employementStatus: EmployementStatus,
+    val certification: Boolean
+)
+
+val teachers = listOf(
+    Teacher("Alexander", 30, EmployementStatus.FULL_TIME, true),
+    Teacher("Minh", 30, EmployementStatus.FULL_TIME, true),
+    Teacher("Pham", 30, EmployementStatus.PART_TIME, true),
+    Teacher("Martin", 30, EmployementStatus.PART_TIME, false),
+    Teacher("Mike", 30, EmployementStatus.SESSIONAL, false),
+)
