e81b8b3f5ddaaf9a211983ed5ac86aea00223ae0
Minh Pham <phamduya@sheridancollege.ca>
2023-06-20 10:05:57 -0400

    Fixed SDK error and completed the GUI of the app

--- app/src/main/java/sheridan/phamduya/midtermexam/MainActivity.kt
+++ app/src/main/java/sheridan/phamduya/midtermexam/MainActivity.kt
@@ -3,13 +3,27 @@ package sheridan.phamduya.midtermexam
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import sheridan.phamduya.midtermexam.data.Teacher
 import sheridan.phamduya.midtermexam.ui.theme.MidtermExamTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MidtermExamApp()
                 }
             }
         }
@@ -34,5 +48,37 @@ class MainActivity : ComponentActivity() {
 fun GreetingPreview() {
     MidtermExamTheme {
 
+    }
+}
+
+@Composable
+fun MidtermExamApp(){
+    val viewModel: MainViewModel = viewModel()
+    val teacherList: List<Teacher> by viewModel.uiState
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 15.dp, vertical = 8.dp)
+    ){
+        items(teacherList) { teacher ->
+            TeacherList(teacher)
+        }
+    }
+}
+
+@Composable
+fun TeacherList(teacher: Teacher){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text("Name: ${teacher.name}", style = MaterialTheme.typography.headlineLarge)
+        Text("Weekly Hours: ${teacher.hour}", style = MaterialTheme.typography.bodyLarge)
+        Text("Employment Status: ${teacher.employementStatus}", style = MaterialTheme.typography.bodyLarge)
+        Text("Certification: ${if (teacher.certification) "Yes" else "No"}", style = MaterialTheme.typography.bodyLarge)
     }
 }
